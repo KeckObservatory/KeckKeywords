7a7bb36ef1ae07552509a12a77da23abe16d4daf
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-07-14 12:06:49 -0500

    progress...

--- keck-keywords/kshow.py
+++ keck-keywords/kshow.py
@@ -1,4 +1,4 @@
-#! /kroot/rel/default/bin/kpython3
+#! /usr/bin/env python
 import urllib.request
 import argparse
 
@@ -9,8 +9,8 @@ parser.add_argument('server',nargs="?",default=None, help='Server')
 parser.add_argument('keyword',nargs="?",default=None, help='Keyword')
 
 
-def show(server, keyword):
-    url = 'http://localhost:5002/keyword/%s/%s' % (server, keyword)
+def kshow(server, keyword):
+    url = 'http://localhost:5002/show/%s/%s' % (server, keyword)
     f = urllib.request.urlopen(url)
     print(f.read().decode('utf-8'))
 
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    show(args.server, args.keyword)
+    kshow(args.server, args.keyword)

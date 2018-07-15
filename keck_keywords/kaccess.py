35dd88fe74ff5b0a3ae87f66d9c6103af15f184b
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-07-14 22:11:22 -0500

    add server

--- keck_keywords/kaccess.py
+++ keck_keywords/kaccess.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 import requests
 
-def kshow(server, keyword):
+def kshow(host, server, keyword):
     """Show the value of a keyword
 
     Reproduces the behaviour of show keywords -s server
 
     Parameters
     ----------
-    server : string
+    host : str
+        Instrument host computer
+    server : str
         Server to be queried
-    keyword : string
+    keyword : str
         Keyword to be queried
 
     Returns
@@ -19,18 +21,20 @@ def kshow(server, keyword):
          Value of the keyword
 
     """
-    url = 'http://localhost:5002/show/%s/%s' % (server, keyword)
+    url = 'http://%s:5002/show/%s/%s' % (host, server, keyword)
     response = requests.get(url)
     return response.json()
 
 
-def kmodify(server, keyword, value):
+def kmodify(host, server, keyword, value):
     """Modify the value of a keyword.
 
     Reproduces the behaviour of modify -s server keyword
 
     Parameters
     ----------
+    host : str
+        Instrument host computer
     server : str
         Server
     keyword : str
@@ -45,18 +49,20 @@ def kmodify(server, keyword, value):
 
 
     """
-    url = 'http://localhost:5002/modify/%s/%s' % (server, keyword)
+    url = 'http://%s:5002/modify/%s/%s' % (host, server, keyword)
     response = requests.post(url,json={"value": value})
     return response.ok, response.reason
 
 
-def kshow_keywords(server):
+def kshow_keywords(host, server):
     """Show the list of keywords for a given server.
 
     Reproduces the behaviour of show -s server keyword
 
     Parameters
     ----------
+    host : str
+        Instrument host computer
     server : str
         Server
 
@@ -66,6 +72,6 @@ def kshow_keywords(server):
          Array of keywords
 
     """
-    url = 'http://localhost:5002/showkeywords/%s' % server
+    url = 'http://%s:5002/showkeywords/%s' % (host, server)
     response = requests.get(url)
     return response.json()

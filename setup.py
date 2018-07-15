aaa532220fc05e05f79b1a36a4dc2712694fadf8
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-07-14 21:46:54 -0500

    small change to new line at the end of file

--- setup.py
+++ setup.py
@@ -4,12 +4,22 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import os,glob
+import os
+import glob
+
 
 # Treat everything in scripts except README.rst as a script to be installed
 scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
            if os.path.basename(fname) != 'README.rst']
 
+# Get some values from the setup.cfg
+try:
+    from ConfigParser import ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+
+conf = ConfigParser()
+conf.read(['setup.cfg'])
 
 config = {
     'description': 'Access to Keck Instrument Keywords',
@@ -25,4 +35,3 @@ config = {
 }
 
 setup(**config)
-

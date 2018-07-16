b016d9aa728539733a964b2f562a714feaee0556
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-07-16 15:23:27 -0700

    first attempt at adding steaming server

--- keyword_server/keyword_server.py
+++ keyword_server/keyword_server.py
@@ -19,8 +19,14 @@ use_graphics = True
 try:
     import pandas as pd
     import holoviews as hv
+    from holoviews.streams import Buffer
     from bokeh.embed import file_html
     from bokeh.resources import CDN
+    from bokeh.plotting import curdoc
+    from tornado import gen
+    from threading import Thread
+    from functools import partial
+
 except ImportError as error:
     use_graphics = False
     warnings.warn("One of the graphics modules is not available. Graphics functions are disabled.")
@@ -128,6 +134,85 @@ def plot_keyword(server,keyword):
     #return render_template("plot.html", keyword_name = keyword, the_div=div, the_script=script)
     #print(html)
     return html
+
+@app.route('/stream')
+def keyword_stream():
+    keyword = 'pressure'
+    server = 'kbvs'
+    pressure = ktl.cache('kt1s', 'tmp1')
+    mykeyword = exptime
+
+    def convert_time(timestamp):
+        return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    initial_y = mykeyword.read(binary=True)
+    example = pd.DataFrame({'x': [convert_time(time.time())], 'y': [initial_y]}, columns=['x', 'y'])
+
+    dfstream = Buffer(example, length=100, index=False)
+
+    @gen.coroutine
+    def update(x, y):
+        global example
+        if y < 0.1:
+            example = pd.DataFrame({'x': x, 'y': 0}, columns=['x', 'y'])
+        else:
+            example = example.append({'x': x, 'y': y}, ignore_index=True)
+        dfstream.send(example)
+        print(example.head())
+
+
+    pressure = ktl.cache('kt1s', 'tmp1')
+    exptime = ktl.cache('kbds', 'elaptime')
+    ttime = ktl.cache('kbds', 'ttime')
+    mykeyword = exptime
+
+    def convert_time(timestamp):
+        return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    initial_y = mykeyword.read(binary=True)
+    example = pd.DataFrame({'x': [convert_time(time.time())], 'y': [initial_y]}, columns=['x', 'y'])
+
+    dfstream = Buffer(example, length=100, index=False)
+
+    doc = curdoc()
+
+    curve_dmap = hv.DynamicMap(hv.Points, streams=[dfstream]).options(color='red', line_width=5, width=1200,
+                                                                      xrotation=90)
+    example = pd.DataFrame({'x': x, 'y': 0}, columns=['x', 'y'])
+    @gen.coroutine
+    def update(x, y):
+        global example
+        example = example.append({'x': x, 'y': y}, ignore_index=True)
+        dfstream.send(example)
+        print(example.head())
+
+    def callback(keyword):
+        global example
+        y = mykeyword.binary
+        time_now = mykeyword.timestamp
+        last_time_stamp = example.iloc[-1]['x']
+        print(last_time_stamp)
+        if last_time_stamp == convert_time(time_now):
+            return
+        doc.add_next_tick_callback(partial(update, x=convert_time(time_now), y=float(y)))
+
+    def start_monitor():
+        mykeyword.callback(callback)
+        mykeyword.monitor()
+        while True:
+            pass
+
+    #doc = hv.renderer('bokeh').server_doc(curve_dmap)
+    renderer = hv.renderer('bokeh')
+    hvplot = renderer.get_plot(myplot).state
+
+    thread = Thread(target=start_monitor)
+    thread.start()
+
+    html = file_html(hvplot, CDN, "Plot: %s from %s" % (keyword, server))
+    return html
+
+
 
 
 @app.route('/')

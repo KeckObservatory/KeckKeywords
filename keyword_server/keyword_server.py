ac7d6bd8ded0170459de47d9648fecfb9f362d17
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-07-18 19:17:33 -1000

    further attempts at a streaming server

--- keyword_server/keyword_server.py
+++ keyword_server/keyword_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, jsonify, request
+from flask import Flask, json, jsonify, request, render_template
 import subprocess
 import sys
 import os
@@ -16,19 +16,26 @@ except ImportError as error:
     print("KTL cannot be imported. The keyword server cannot be started.")
     print("Make sure RELDIR is defined or add the ktl location to your PYTHONPATH")
     print(error)
-    sys.exit(1)
+    #sys.exit(1)
 use_graphics = True
 try:
     import pandas as pd
     import holoviews as hv
     from holoviews.streams import Buffer
-    from bokeh.embed import file_html
+    from bokeh.embed import file_html, server_document
     from bokeh.resources import CDN
     from bokeh.plotting import curdoc, figure
-    from bokeh.models import ColumnDataSource, AjaxDataSource, DatetimeTickFormatter
+    from bokeh.models import ColumnDataSource, AjaxDataSource, DatetimeTickFormatter, Slider
+    from bokeh.server.server import Server
+    from bokeh.layouts import column
+    from bokeh.themes import Theme
+
     from tornado import gen
+    from tornado.ioloop import IOLoop
     from threading import Thread
     from functools import partial
+    from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
+
 
 except ImportError as error:
     use_graphics = False
@@ -144,8 +151,35 @@ def plot_keyword(server,keyword):
     #print(html)
     #return html
 
-@app.route('/stream')
-def keyword_stream():
+
+
+######## STREAM TESTING
+
+def modify_doc(doc):
+    df = sea_surface_temperature.copy()
+    source = ColumnDataSource(data=df)
+
+    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
+                  title="Sea Surface Temperature at 43.18, -70.43")
+    plot.line('time', 'temperature', source=source)
+
+    def callback(attr, old, new):
+        if new == 0:
+            data = df
+        else:
+            data = df.rolling('{0}D'.format(new)).mean()
+        source.data = ColumnDataSource(data=data).data
+
+    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
+    slider.on_change('value', callback)
+
+    doc.add_root(column(slider, plot))
+
+    doc.theme = Theme(filename="theme.yaml")
+
+
+#@app.route('/stream')
+def keyword_stream(doc):
     keyword = 'temperature'
     server = 'kt1s'
     tmp1 = ktl.cache('kt1s', 'tmp1')
@@ -163,7 +197,7 @@ def keyword_stream():
     dfstream = Buffer(example, length=100, index=False)
     curve_dmap = hv.DynamicMap(hv.Points, streams=[dfstream]).options(color='red', line_width=5, width=1200, xrotation=90)
 
-    doc = curdoc()
+    #doc = curdoc()
 
     @gen.coroutine
     def update(x, y):
@@ -193,7 +227,7 @@ def keyword_stream():
         #    return
         print("calling update")
         update(convert_time(time_now), float(y))
-        #doc.add_next_tick_callback(partial(update,  x=convert_time(time_now), y=float(y)))
+        doc.add_next_tick_callback(partial(update,  x=convert_time(time_now), y=float(y)))
 
     def start_monitor():
         global example
@@ -207,16 +241,36 @@ def keyword_stream():
 
     renderer = hv.renderer('bokeh')
     hvplot = renderer.get_plot(curve_dmap).state
+    doc.add_root(hvplot)
+    start_monitor()
+    #thread = Thread(target=start_monitor)
+    #thread.start()
 
-    thread = Thread(target=start_monitor)
-    thread.start()
+    #html = file_html(hvplot, CDN, "Plot: %s from %s" % (keyword, server))
+    #return html
 
-    html = file_html(hvplot, CDN, "Plot: %s from %s" % (keyword, server))
-    return html
+
+
+    
+@app.route('/teststream', methods=['GET'])
+def bkapp_page():
+    script = server_document('http://localhost:5006/bkapp')
+    return render_template("embed.html", script=script, template="Flask")
+
+def bk_worker():
+    # Can't pass num_procs > 1 in this configuration. If you need to run multiple
+    # processes, see e.g. flask_gunicorn_embed.py
+    server = Server({'/bkapp': keyword_stream}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
+    server.start()
+    server.io_loop.start()
 
 @app.route('/')
 def index():
     return("Hello, welcome!")
 
+    
+from threading import Thread
+Thread(target=bk_worker).start()
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(port=8000, debug=False)
